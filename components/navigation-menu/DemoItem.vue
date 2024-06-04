06e82e1e1be0dcf03ed33f53a5b1d22cfdd49dc1
Dian Pratama <>
2024-06-04 23:20:54 +0700

    feat: add new page component navigation menu

---
+++ components/navigation-menu/DemoItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  href?: string
+}>()
+</script>
+
+<template>
+  <li>
+    <NavigationMenuLink as-child>
+      <a
+        :href="href"
+        :class="cn(
+          'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+          $attrs.class ?? '',
+        )"
+      >
+        <div class="text-sm font-medium leading-none">{{ title }}</div>
+        <p class="line-clamp-2 text-sm text-muted-foreground leading-snug">
+          <slot />
+        </p>
+      </a>
+    </NavigationMenuLink>
+  </li>
+</template>
