9ca7891159fd5471d0317475019b4a933f39e5f1
Dian Pratama <>
2024-06-04 23:50:49 +0700

    feat: add new page component pin input

---
+++ components/ui/pin-input/PinInputSeparator.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps, useForwardProps } from 'radix-vue'
+
+const props = defineProps<PrimitiveProps>()
+const forwardedProps = useForwardProps(props)
+</script>
+
+<template>
+  <Primitive v-bind="forwardedProps">
+    <slot>
+      <Icon name="i-radix-icons-dash" />
+    </slot>
+  </primitive>
+</template>
