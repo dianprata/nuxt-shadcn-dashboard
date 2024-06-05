72e4d567ce5d1602f72ce0f1d19ef51603f71dc1
Dian Pratama <>
2024-06-05 10:38:06 +0700

    feat: add new page component sonner

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+import { Sonner } from '@/components/ui/sonner'
+</script>
+
 <template>
   <NuxtLayout vaul-drawer-wrapper>
     <NuxtPage />
   </NuxtLayout>
 
   <Toaster />
+  <Sonner class="pointer-events-auto" />
 </template>
