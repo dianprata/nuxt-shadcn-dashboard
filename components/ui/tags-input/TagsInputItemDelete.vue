affd5a2c10a439eb5ad6ee3a04a4cc0c71f08a78
Dian Pratama <>
2024-06-05 12:27:08 +0700

    feat: add new page component tags input

---
+++ components/ui/tags-input/TagsInputItemDelete.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from 'vue'
+import { TagsInputItemDelete, type TagsInputItemDeleteProps, useForwardProps } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<TagsInputItemDeleteProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <TagsInputItemDelete v-bind="forwardedProps" :class="cn('flex rounded bg-transparent mr-1', props.class)">
+    <slot>
+      <Icon name="i-radix-icons-cross-2" class="h-4 w-4" />
+    </slot>
+  </TagsInputItemDelete>
+</template>
