8860824db830ccfaa0233b750bdcd0af4b6a31de
Dian Pratama <>
2024-06-05 12:37:24 +0700

    feat: add new page component toast

---
+++ pages/components/toast.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { useToast } from '~/components/ui/toast'
+
+const breadcrumbLinks = ref([
+  {
+    title: 'Home',
+    href: '/',
+  },
+  {
+    title: 'Components',
+    href: '#',
+  },
+  {
+    title: 'Toast',
+    href: '/components/toast',
+  },
+])
+
+const { toast } = useToast()
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <BaseBreadcrumbCustom :links="breadcrumbLinks" />
+    <div class="grid gap-2">
+      <h2 class="text-3xl font-bold tracking-tight">
+        Toast
+      </h2>
+      <p class="text-muted-foreground">
+        A succinct message that is displayed temporarily.
+      </p>
+      <div class="flex gap-2">
+        <Button size="xs" variant="outline" class="text-xs" as-child>
+          <NuxtLink
+            to="https://www.shadcn-vue.com/docs/components/toast"
+            external
+            target="_blank"
+          >
+            <span class="i-radix-icons-code mr-2" />
+            Component Source
+          </NuxtLink>
+        </Button>
+        <Button size="xs" variant="outline" class="text-xs" as-child>
+          <NuxtLink
+            to="https://www.radix-vue.com/components/toast"
+            external
+            target="_blank"
+          >
+            Primitive API Reference
+          </NuxtLink>
+        </Button>
+      </div>
+    </div>
+    <div class="grid gap-4 md:grid-cols-2">
+      <Card class="w-full">
+        <CardHeader>
+          <CardTitle>Basic</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div class="min-h-100px w-full flex items-center justify-center gap-4 md:min-h-200px">
+            <Button
+              variant="outline" @click="() => {
+                toast({
+                  title: 'Scheduled: Catch up',
+                  description: 'Friday, February 10, 2023 at 5:57 PM',
+                });
+              }"
+            >
+              Add to calendar
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
