d00af854fc3155cf8dec9071298092c4e9e8bfb4
Dian Pratama <>
2024-06-09 15:25:25 +0700

    feat(dark toggle): update with system

--- components/DarkToggle.vue
+++ components/DarkToggle.vue
@@ -8,14 +8,42 @@ useHead({
     content: () => color.value === 'dark' ? '#000000' : '#ffffff',
   }],
 })
-
-function toggleDark() {
-  color.preference = color.value === 'dark' ? 'light' : 'dark'
-}
 </script>
 
 <template>
-  <Button variant="ghost" size="icon" class="rounded-full" title="Toggle Color Scheme" aria-label="button dark toggle" @click="toggleDark">
-    <div class="i-ph-sun-dim-duotone dark:i-ph-moon-stars-duotone" />
-  </Button>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="ghost" size="icon" class="rounded-full" title="Toggle Color Scheme" aria-label="button dark toggle">
+        <div class="i-ph-sun-dim-duotone dark:i-ph-moon-stars-duotone" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuGroup>
+        <DropdownMenuItem
+          flex="~ gap-2"
+          :class="{ 'bg-accent text-accent-foreground': color.preference === 'light' }"
+          @click="color.preference = 'light'"
+        >
+          <Icon name="i-ph-sun-dim-duotone" size="16" />
+          <span>Light</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          flex="~ gap-2"
+          :class="{ 'bg-accent text-accent-foreground': color.preference === 'dark' }"
+          @click="color.preference = 'dark'"
+        >
+          <Icon name="i-ph-moon-stars-duotone" size="16" />
+          <span>Dark</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          flex="~ gap-2"
+          :class="{ 'bg-accent text-accent-foreground': color.preference === 'system' }"
+          @click="color.preference = 'system'"
+        >
+          <Icon name="i-lucide-monitor" size="16" />
+          <span>System</span>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
