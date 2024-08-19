cc85d181dd292fda1876c240318e6254e84e475f
Dian Pratama <>
2024-08-19 20:50:01 +0700

    fix(layout): implement theme on layout blank

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+const store = useNavbar()
+const { theme, radius } = useCustomize()
 
+useServerHead({
+  bodyAttrs: {
+    class: `theme-${theme.value}`,
+    style: `--radius: ${radius.value}rem;`,
+  },
+})
+
+const router = useRouter()
+
+defineShortcuts({
+  'Meta_B': () => store.toggle(),
+  'G-H': () => router.push('/'),
+  'G-E': () => router.push('/email'),
+})
 </script>
 
 <template>
