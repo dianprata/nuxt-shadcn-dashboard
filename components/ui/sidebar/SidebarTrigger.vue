d9a3baa85aac145e71ef20089ae185f0ff696581
Dian Pratama <dianpratama2@gmail.com>
2024-12-15 01:11:46 +0700

    fix: remove tooltip on SidebarTrigger

--- components/ui/sidebar/SidebarTrigger.vue
+++ components/ui/sidebar/SidebarTrigger.vue
@@ -10,27 +10,17 @@ const props = defineProps<{
 }>()
 
 const { toggleSidebar } = useSidebar()
-
-const { metaSymbol } = useShortcuts()
 </script>
 
 <template>
-  <Tooltip>
-    <TooltipTrigger as-child>
-      <Button
-        data-sidebar="trigger"
-        variant="ghost"
-        size="icon"
-        :class="cn('h-4 w-4', props.class)"
-        @click="toggleSidebar"
-      >
-        <PanelLeft />
-        <span class="sr-only">Toggle Sidebar</span>
-      </Button>
-    </TooltipTrigger>
-    <TooltipContent class="p-1 space-x-0.5">
-      <BaseKbd>{{ metaSymbol }}</BaseKbd>
-      <BaseKbd>B</BaseKbd>
-    </TooltipContent>
-  </Tooltip>
+  <Button
+    data-sidebar="trigger"
+    variant="ghost"
+    size="icon"
+    :class="cn('h-4 w-4', props.class)"
+    @click="toggleSidebar"
+  >
+    <PanelLeft />
+    <span class="sr-only">Toggle Sidebar</span>
+  </Button>
 </template>
