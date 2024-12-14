923b686237f5f5bc00728e559efb6ab8bf5b31f5
Dian Pratama <dianpratama2@gmail.com>
2024-12-14 23:07:54 +0700

    feat: update page error

--- error.vue
+++ error.vue
@@ -2,20 +2,26 @@
 definePageMeta({
   layout: 'blank',
 })
+
+const router = useRouter()
 </script>
 
 <template>
   <div class="h-svh">
     <div class="m-auto flex flex-col gap-2 h-full w-full justify-center items-center">
       <h1 class="text-[7rem] font-bold leading-tight">
-        401
+        404
       </h1>
-      <span class="font-medium">Unauthorized Access</span>
+      <span class="font-medium">Oops! Page Not Found!</span>
       <p class="text-muted-foreground text-center">
-        Please log in with the appropriate credentials <br> to access this resource.
+        It seems like the page you're looking for <br>
+        does not exist or might have been removed.
       </p>
       <div class="mt-6 flex gap-4">
-        <Button @click="$router.push('/')">
+        <Button variant="outline" @click="router.back()">
+          Go Back
+        </Button>
+        <Button @click="router.push('/')">
           Back to Home
         </Button>
       </div>
