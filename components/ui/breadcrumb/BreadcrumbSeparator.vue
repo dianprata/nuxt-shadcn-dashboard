ede8fc309a53e08703c14116f7f4800622a58ffe
Peyton Creery <44987569+phcreery@users.noreply.github.com>
2025-01-07 12:27:15 -0600

    fix BreadcrumbSeparator vertical alignment

--- components/ui/breadcrumb/BreadcrumbSeparator.vue
+++ components/ui/breadcrumb/BreadcrumbSeparator.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
   <li
     role="presentation"
     aria-hidden="true"
-    :class="cn('[&>svg]:size-3.5', props.class)"
+    :class="cn('inline-flex items-center [&>svg]:size-3.5', props.class)"
   >
     <slot>
       <ChevronRight />
